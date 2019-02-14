54062b07b854058539cae87e19a1bfd4e5b67fd3
Elise Phan <aliceconnic@gmail.com>
2019-02-14 03:34:28 +0000

    Done.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,10 @@ class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
 
+  def users_attributes=(users_attributes)
+    user_attributes.values.each do |user_attribute|
+    user = User.find_or_create_by(user_attribute)
+    self.users << user 
+    end
+  end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,7 @@ class Post < ActiveRecord::Base
   has_many :categories, through: :post_categories
   has_many :comments
   has_many :users, through: :comments
+  has_many :comments
   accepts_nested_attributes_for :categories
 
   def categories_attributes=(category_attributes)
@@ -10,6 +11,27 @@ class Post < ActiveRecord::Base
       category = Category.find_or_create_by(category_attribute)
       self.categories << category 
     end
+  end
+  
+  def comment_contents=(comments)
+    comments.each do |content|
+      if content.strip != ''
+        self.comments.build(content: content)
+      end
+    end
+  end
+  
+  def comment_contents
+    self.comments.map(&:content)
+  end
+  
+  def user_username
+    self.try(:user).try(:username)
+  end
+  
+  def user_username=(username)
+    user = User.find_or_create_by(username: username)
+    self.user = user
   end
 
 end
