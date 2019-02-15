91caa059419109016bfad2032f30b1497e744b8b
Elise Phan <aliceconnic@gmail.com>
2019-02-15 20:40:09 +0000

    Automatically backed up by Learn

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,7 +2,7 @@ class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
 
-  def users_attributes=(users_attributes)
+  def user_attributes=(user_attributes)
     user_attributes.values.each do |user_attribute|
     user = User.find_or_create_by(user_attribute)
     self.users << user 
