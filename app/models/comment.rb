0757c61656dae3f8c14e0014867872ffcb168918
Elise Phan <aliceconnic@gmail.com>
2019-02-15 20:00:58 +0000

    Automatically backed up by Learn

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,8 +1,7 @@
 class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
-  accepts_nested_attributes_for :users
-  
+
   def users_attributes=(users_attributes)
     user_attributes.values.each do |user_attribute|
     user = User.find_or_create_by(user_attribute)
